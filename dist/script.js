4f1a384a1391de55ea01208e2be2561d5785088a
Neal Rossov <nealrossov@gmail.com>
2023-10-03 07:28:53 +0300

    10/3/2023

--- dist/script.js
+++ dist/script.js
@@ -778,3 +778,32 @@ var i = 0;
 while (i < 10) {
     console.log(i);
 }
+// Task 4.
+var stars = document.getElementsByClassName("star");
+var output = document.getElementById("output");
+// Funtion to update rating
+function gfg(n) {
+    remove();
+    for (var i_3 = 0; i_3 < n; i_3++) {
+        if (n == 1)
+            cls = "one";
+        else if (n == 2)
+            cls = "two";
+        else if (n == 3)
+            cls = "three";
+        else if (n == 4)
+            cls = "four";
+        else if (n == 5)
+            cls = "five";
+        stars[i_3].className = "star " + cls;
+    }
+    output.innerText = "Rating is: " + n + "/5";
+}
+// To remove the pre-applied styling
+function remove() {
+    var i = 0;
+    while (i < 5) {
+        stars[i].className = "star";
+        i++;
+    }
+}
